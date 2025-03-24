4af107fd8506590e96abee8f94dc59e647b916e5
Mike Edmunds <medmunds@gmail.com>
2025-03-23 18:26:15 -0700

    Fix failing test detection
    Make the CI workflow test job fail if tests aren't successful.

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,6 +1,8 @@
+import sys
 import unittest
 
 if __name__ == "__main__":
     test_suite = unittest.defaultTestLoader.discover(".", pattern="test_*.py")
     runner = unittest.TextTestRunner()
-    runner.run(test_suite)
+    result = runner.run(test_suite)
+    sys.exit(not result.wasSuccessful())
